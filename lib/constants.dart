7f702078f5a2db0de9cbe42a0278627072183dd0
shinjini25 <shinjinibh2503@gmail.com>
2022-06-24 21:45:36 +0530

    confirm video screen done

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,13 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:videozen/views/screens/auth/add_video.dart';
 import 'package:videozen/views/screens/auth/home_screen.dart';
 import 'controllers/auth_controllers.dart';
 
 const pages = [
   Text("home"),
   Text("Search"),
-  Text("Add page screen"),
+  AddVideoScreen(),
   Text("Messages"),
   Text("Profile"),
 ];
